db69a33fae10a6cbac2398adf4ca509c9951df23
Alex Bieth <alex.bieth@gmail.com>
2018-08-19 13:58:57 +0200

    corrections part3

--- Public/script.js
+++ Public/script.js
@@ -27,3 +27,23 @@ document.addEventListener("mouseup", function() {
     canvas.removeEventListener("mousemove", move);
     sig.value = canvas.toDataURL();
 });
+
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {
+//     myFunction();
+// };
+//
+// // Get the navbar
+// var nav = document.getElementsByName("nav");
+//
+// // Get the offset position of the navbar
+// var sticky = nav.offsetTop;
+//
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//         nav.classList.add("sticky");
+//     } else {
+//         nav.classList.remove("sticky");
+//     }
+// }
