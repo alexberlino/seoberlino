4ae5019404fc34835c657ad504ee51882f7a1ee3
Alex Bieth <alex.bieth@gmail.com>
2018-08-26 18:00:40 +0200

    fix

---
+++ SQL/signatures.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS signatures;
+CREATE TABLE signatures (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER UNIQUE NOT NULL REFERENCES users(id),
+    signature TEXT NOT NULL
+);
+
+
+
+    -- VARCHAR varying characters, but not exceding 15 characters
+
+
+
+
+-- INSERT INTO actors (name, age, 'number of oscars')
+-- VALUES ('Leonardo DiCaprio', 41, 1);
+-- SELECT name FROM signedby
